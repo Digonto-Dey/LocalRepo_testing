83309064603326bfb51b55fa44a63d299b11898d
Digonto Dey <deydigonto@gmail.com>
2026-04-20 02:00:02 +0600

    14th line

--- calculator.r
+++ calculator.r
@@ -5,14 +5,14 @@ calculator <- function(x, y, operation) {
         "subtract" = x - y,
         "multiply" = x * y,
         "divide" = if (y != 0) x / y else "Error: Division by zero",
-        "power" = x ^ y,
+        "power" = x^y,
         "Invalid operation"
     )
 }
 
 # Example usage:
-print(calculator(10, 5, "add"))        # 15
-print(calculator(10, 5, "subtract"))   # 5
-print(calculator(10, 5, "multiply"))   # 50
-print(calculator(10, 5, "divide"))     # 2
-print(calculator(10, 5, "power"))      # 100000
+print(calculator(10, 155, "add")) # 15
+print(calculator(10, 5, "subtract")) # 5
+print(calculator(10, 5, "multiply")) # 50
+print(calculator(10, 5, "divide")) # 2
+print(calculator(10, 5, "power")) # 100000
